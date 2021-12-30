3ab9018c699bd56f47f15b2292c5c121afdebaca
Ben Wallace <bencwallace@gmail.com>
2021-12-29 22:17:32 -0500

    reorder visit methods

--- simplify/simplifier.py
+++ simplify/simplifier.py
@@ -35,15 +35,13 @@ class Simplifier(ast.NodeTransformer):
     def new_environment(self, name):
         return EnvHandler(self, name)
 
+    # STATEMENTS #
+
     def visit_FunctionDef(self, node):
         # TODO: Add decorators, etc.
         # TODO: Check if return value can be extracted
         with self.new_environment(node.name):
             return super().generic_visit(node)
-
-    def visit_Call(self, node):
-        # TODO: Add kwargs
-        pass
 
     # TODO: Similar for delete (and others?)
     def visit_Assign(self, node):
@@ -53,12 +51,15 @@ class Simplifier(ast.NodeTransformer):
                 self._environment[t.id] = val.value
         return None
 
-    def visit_Name(self, node):
-        if isinstance(node.ctx, ast.Load):
-            if node.id in self._environment:
-                return ast.Constant(self._environment[node.id])
-            return node
-        raise NotImplementedError(f"Name expression of type {type(node.ctx).__name__} not supported.")
+    def visit_If(self, node):
+        test = self.visit(node.test)
+        if not isinstance(test, ast.Constant):
+            return ast.If(test, self._visit_nodes(node.body), self._visit_nodes(node.orelse))
+        if test.value:
+            return self._visit_nodes(node.body)
+        return self._visit_nodes(node.orelse)
+
+    # EXPRESSIONS #
 
     # TODO: Similar for other statements/expressions
     def visit_BinOp(self, node):
@@ -68,6 +69,14 @@ class Simplifier(ast.NodeTransformer):
         if not isinstance(left, ast.Constant) or not isinstance(right, ast.Constant):
             return ast.BinOp(left, node.op, right)
         return ast.Constant(BIN_OPS[type(node.op)](left.value, right.value))
+
+    def visit_IfExp(self, node):
+        test = self.visit(node.test)
+        if not isinstance(test, ast.Constant):
+            return ast.IfExp(test, self.visit(node.body), self.visit(node.orelse))
+        if test.value:
+            return self.visit(node.body)
+        return self.visit(node.orelse)
 
     def visit_Compare(self, node):
         left = self.visit(node.left)
@@ -80,18 +89,13 @@ class Simplifier(ast.NodeTransformer):
             result = result and CMP_OPS[type(op)](l.value, r.value)
         return ast.Constant(result)
 
-    def visit_IfExp(self, node):
-        test = self.visit(node.test)
-        if not isinstance(test, ast.Constant):
-            return ast.IfExp(test, self.visit(node.body), self.visit(node.orelse))
-        if test.value:
-            return self.visit(node.body)
-        return self.visit(node.orelse)
+    def visit_Call(self, node):
+        # TODO: Add kwargs
+        pass
 
-    def visit_If(self, node):
-        test = self.visit(node.test)
-        if not isinstance(test, ast.Constant):
-            return ast.If(test, self._visit_nodes(node.body), self._visit_nodes(node.orelse))
-        if test.value:
-            return self._visit_nodes(node.body)
-        return self._visit_nodes(node.orelse)
+    def visit_Name(self, node):
+        if isinstance(node.ctx, ast.Load):
+            if node.id in self._environment:
+                return ast.Constant(self._environment[node.id])
+            return node
+        raise NotImplementedError(f"Name expression of type {type(node.ctx).__name__} not supported.")

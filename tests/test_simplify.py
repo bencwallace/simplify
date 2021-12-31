cfbcbc43e253f53c59b6aa664ea283f6b95e709b
Ben Wallace <bencwallace@gmail.com>
2021-12-31 15:43:04 -0500

    add test for return

--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -65,3 +65,15 @@ def test_del(source, result):
 )
 def test_bool(source, result):
     assert result == transform_source(source)
+
+
+@pytest.mark.parametrize(
+    "source, result",
+    [
+        ("def f(): return 1", "def f():\n    return 1"),
+        ("def f(): return", "def f():\n    return"),
+        ("def f(): return 1 + 1", "def f():\n    return 2"),
+    ],
+)
+def test_return(source, result):
+    assert result == transform_source(source)
